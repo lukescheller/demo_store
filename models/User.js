3924b244316dc1f05e85e8e8b5022410eb560e43
Luke Scheller <lukescheller1991@outlook.com>
2022-02-28 10:02:37 -0600

    later commit

--- models/User.js
+++ models/User.js
@@ -15,6 +15,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  purchases: {
+    type: Array,
+    default: [],
+  },
   date: {
     type: Date,
     default: Date.now,
